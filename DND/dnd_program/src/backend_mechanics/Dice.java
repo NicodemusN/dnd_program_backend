4b8b0afb13f36703593436a505adb4eb0d99849f
NicodemusN <87670335+NicodemusN@users.noreply.github.com>
2021-08-28 16:47:06 +0300

    Updated Dice.java
    The code now lets you choose the amount of dices to throw between 4,6,8,10,12 and 20 sided dice.

--- DND/dnd_program/src/backend_mechanics/Dice.java
+++ DND/dnd_program/src/backend_mechanics/Dice.java
@@ -1,14 +1,76 @@
 package backend_mechanics;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Dice {
-
-	public Dice() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 System.out.println("Enter the number of sides for a dice: ");
+	        Scanner sc = new Scanner(System.in);
+	        int n = sc.nextInt();
+	        System.out.println("Enter the number of dices: ");
+	        int m = sc.nextInt();
+	        Random rand = new Random();
+	        if (n==4){
+	        	 do 
+	 	        {
+	 	            System.out.println("The values on dice are: ");
+	 	            for(int i=0; i<m; i++)
+	 	                System.out.println(rand.nextInt(4)+1);
+	 	 
+	 	            System.out.println("Continue: true/false");
+	 	        } while (sc.nextBoolean() == true);
+	 	        sc.close();
+	        } else if (n==6) {
+	        	do 
+	 	        {
+	 	            System.out.println("The values on dice are: ");
+	 	            for(int i=0; i<m; i++)
+	 	                System.out.println(rand.nextInt(6)+1);
+	 	 
+	 	            System.out.println("Continue: true/false");
+	 	        } while (sc.nextBoolean() == true);
+	 	        sc.close();
+	        } else if (n==8) {
+	        	do 
+	 	        {
+	 	            System.out.println("The values on dice are: ");
+	 	            for(int i=0; i<m; i++)
+	 	                System.out.println(rand.nextInt(8)+1);
+	 	 
+	 	            System.out.println("Continue: true/false");
+	 	        } while (sc.nextBoolean() == true);
+	 	        sc.close();
+	        } else if (n==10) {
+	        	do 
+	 	        {
+	 	            System.out.println("The values on dice are: ");
+	 	            for(int i=0; i<m; i++)
+	 	                System.out.println(rand.nextInt(10)+1);
+	 	 
+	 	            System.out.println("Continue: true/false");
+	 	        } while (sc.nextBoolean() == true);
+	 	        sc.close();
+	        } else if (n==12) {
+	        	do 
+	 	        {
+	 	            System.out.println("The values on dice are: ");
+	 	            for(int i=0; i<m; i++)
+	 	                System.out.println(rand.nextInt(12)+1);
+	 	 
+	 	            System.out.println("Continue: true/false");
+	 	        } while (sc.nextBoolean() == true);
+	 	        sc.close();
+	        } else if (n==20) {
+	        	do 
+	 	        {
+	 	            System.out.println("The values on dice are: ");
+	 	            for(int i=0; i<m; i++)
+	 	                System.out.println(rand.nextInt(20)+1);
+	 	 
+	 	            System.out.println("Continue: true/false");
+	 	        } while (sc.nextBoolean() == true);
+	 	        sc.close();
+	        }
 	}
-
 }
