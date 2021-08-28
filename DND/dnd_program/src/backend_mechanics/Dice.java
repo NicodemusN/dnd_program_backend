daa30ece9a3268894f8d3c91e22f0ff3c37068e9
NicodemusN <87670335+NicodemusN@users.noreply.github.com>
2021-08-28 12:46:08 +0300

    Add files via upload

---
+++ DND/dnd_program/src/backend_mechanics/Dice.java
@@ -0,0 +1,14 @@
+package backend_mechanics;
+
+public class Dice {
+
+	public Dice() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
