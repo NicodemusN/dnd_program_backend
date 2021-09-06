cbc71c287e5690cbff18dfb1f4f635f2f9d94524
NicodemusN <87670335+NicodemusN@users.noreply.github.com>
2021-09-06 18:10:27 +0300

    Update Dice.java

--- DND/dnd_program/src/backend_mechanics/Dice.java
+++ DND/dnd_program/src/backend_mechanics/Dice.java
@@ -11,66 +11,15 @@ public class Dice {
 	        System.out.println("Enter the number of dices: ");
 	        int m = sc.nextInt();
 	        Random rand = new Random();
-	        if (n==4){
-	        	 do 
-	 	        {
-	 	            System.out.println("The values on dice are: ");
-	 	            for(int i=0; i<m; i++)
-	 	                System.out.println(rand.nextInt(4)+1);
-	 	 
-	 	            System.out.println("Continue: true/false");
-	 	        } while (sc.nextBoolean() == true);
-	 	        sc.close();
-	        } else if (n==6) {
-	        	do 
-	 	        {
-	 	            System.out.println("The values on dice are: ");
-	 	            for(int i=0; i<m; i++)
-	 	                System.out.println(rand.nextInt(6)+1);
-	 	 
-	 	            System.out.println("Continue: true/false");
-	 	        } while (sc.nextBoolean() == true);
-	 	        sc.close();
-	        } else if (n==8) {
-	        	do 
-	 	        {
-	 	            System.out.println("The values on dice are: ");
-	 	            for(int i=0; i<m; i++)
-	 	                System.out.println(rand.nextInt(8)+1);
-	 	 
-	 	            System.out.println("Continue: true/false");
-	 	        } while (sc.nextBoolean() == true);
-	 	        sc.close();
-	        } else if (n==10) {
-	        	do 
-	 	        {
-	 	            System.out.println("The values on dice are: ");
-	 	            for(int i=0; i<m; i++)
-	 	                System.out.println(rand.nextInt(10)+1);
-	 	 
-	 	            System.out.println("Continue: true/false");
-	 	        } while (sc.nextBoolean() == true);
-	 	        sc.close();
-	        } else if (n==12) {
-	        	do 
-	 	        {
-	 	            System.out.println("The values on dice are: ");
-	 	            for(int i=0; i<m; i++)
-	 	                System.out.println(rand.nextInt(12)+1);
-	 	 
-	 	            System.out.println("Continue: true/false");
-	 	        } while (sc.nextBoolean() == true);
-	 	        sc.close();
-	        } else if (n==20) {
-	        	do 
-	 	        {
-	 	            System.out.println("The values on dice are: ");
-	 	            for(int i=0; i<m; i++)
-	 	                System.out.println(rand.nextInt(20)+1);
-	 	 
-	 	            System.out.println("Continue: true/false");
-	 	        } while (sc.nextBoolean() == true);
-	 	        sc.close();
-	        }
+	        
+	        do 
+ 	        {
+ 	            System.out.println("The values on dice are: ");
+ 	            for(int i=0; i<m; i++)
+ 	                System.out.println(rand.nextInt(n)+1);
+ 	 
+ 	            System.out.println("Continue: true/false");
+ 	        } while (sc.nextBoolean() == true);
+ 	        sc.close();
 	}
 }
